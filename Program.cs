200427332ac2cb83e3b8e4fad97321b4dfadc884
SyntaxNate <new-shutter.nbt@gmail.com>
2025-05-01 23:51:42 -0400

    Initial C# console app

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program 
+{
+    static void Main(string[] args)
+    {
+                Console.WriteLine("Welcome to C#, my friend.");
+                Console.Write("What is your name? ");
+                string name = Console.ReadLine();
+
+                Console.ReadLine();
+
+                Console.WriteLine($"Nice to meet you, {name}! You're officially a C# developer.");
+
+    }
+}
